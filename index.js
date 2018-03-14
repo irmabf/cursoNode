c69946f1f828c939c929af7190a15878d6bd44b2
Irma <irblancofdz@gmail.com>
2018-03-14 16:33:43 +0100

    Add HTTP PUT request, refactor, and add function for validate input, use object restructuring

--- index.js
+++ index.js
@@ -10,18 +10,55 @@ const courses = [
   { id: 3, name: 'course3' },
 ];
 
-app.get('/', (req, res) =>{
-  res.send('Hello world');
+// Post to the collection of courses
+app.post('/api/courses', (req, res) => {
+   const { error } = validateCourse(req.body); 
+   if(error){
+     res.status(400).send(result.error.details[0].message);
+     return; 
+   }
+   const course = {
+     id: courses.length + 1,
+     name: req.body.name, 
+   };
+   courses.push(course);
+   res.send(course);
+ 
+ });
+ 
+
+//Logic for updating a course
+
+app.put('/api/courses/:id', (req, res) =>{
+  const course = courses.find( c => c.id === parseInt(req.params.id));
+   if (!course) res.status(404).send('The course with the given id was not found');
+
+  //const result = validateCourse(req.body);
+  const { error } = validateCourse(req.body); 
+  if(error){
+
+    res.status(400).send(error.details[0].message);
+    return; 
+  }
+
+  //Update the course
+  course.name = req.body.name;
+  //Return the updated course to the client
+  res.send(course);
 });
+
+//Function for validate course input
+function validateCourse(course) {
+  const schema = {
+    name: Joi.string().min(3).required()
+  };
+
+  return Joi.validate(course, schema)
+}
 
 
 //Handiling HTTP GET REQUESTS
 //Now I have 2 enpdpoins:
-
-//One to get all the courses
-app.get('/api/courses', (req, res) =>{
-  res.send(courses);
-});
 
 //One to get one course
 app.get('/api/courses/:id', (req, res) =>{
@@ -32,32 +69,24 @@ app.get('/api/courses/:id', (req, res) =>{
   res.send(course);
 });
 
-// post to the collection of courses
-app.post('/api/courses', (req, res) => {
- //usin Joi class, (provided by joi module), for add input validation for the client
-  const schema = {
-    name: Joi.string().min(3).required()
-  };
 
-  const result = Joi.validate(req.body, schema);
-
-
-  if(result.error){
-    //400, bad request 
-    res.status(400).send(result.error.details[0].message);
-    return; //we return because we dont want the rest of the function to get executed
-  }
-
-  const course = {
-    id: courses.length + 1,
-    name: req.body.name, 
-  };
-
-  courses.push(course);
-
-  res.send(course);
-
+//One to get all the courses
+app.get('/api/courses', (req, res) =>{
+  res.send(courses);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 const port = process.env.PORT || 3000;
